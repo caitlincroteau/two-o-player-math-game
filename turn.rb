9f91caa0a0752d48d5e01e6cb4a78b65cbbfc062
Caitlin <cjcroteau@gmail.com>
2022-08-23 23:13:34 +0000

    add turn class file

---
+++ turn.rb
@@ -0,0 +1,14 @@
+# require './player.rb'
+
+class Turn
+  def initialize()
+    puts "-----New Turn-----"
+  end
+
+  #manage current player
+  #manage current question
+  #output score
+
+end
+
+turn1 = Turn.new()

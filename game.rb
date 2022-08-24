c9b09b51f16172438d280e3d3796db765748e2ec
Caitlin <cjcroteau@gmail.com>
2022-08-24 19:31:43 +0000

    add game loop, question, game-over methods

--- game.rb
+++ game.rb
@@ -1,15 +1,51 @@
+require './player.rb'
+
 class Game
   def initialize(p1, p2)
-    @answer = ""
-    @current_player = ""
+    @current_player = p1
     @current_question = ""
-    @current_score = ""
+    @game_over = 0
+
     puts "You've initialized a game!"
     
   end
 
-  def start
-    puts "game is starting"
+  def start()
+    while @game_over == 0
+    
+    @correct_answer = self.generate_question(@current_player)
+    @input = gets.chomp
+      if @input == @correct_answer
+        puts "that's correct!"
+        puts @current_player.score
+      else
+        puts "that's incorrect!"
+        @current_player.score -= 1
+        puts @current_player.score
+      end
+
+      if @current_player.score == 0
+        @game_over = 1
+      end
+
+      if @game_over == 1
+        self.game_over()
+      else
+        puts "---NEW TURN---"
+      end
+      #switch players here
+    end    
+  end
+
+  def generate_question(player)
+    puts "generating question for #{player.name}"
+    puts "what is 5 + 2?"
+    return "7"
+  
+  end
+
+  def game_over()
+    puts "GAME OVER"
   end
 
   # def current_player()

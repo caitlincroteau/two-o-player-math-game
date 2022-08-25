12e825bee92322417a99dc825ed817f574e50765
Caitlin <cjcroteau@gmail.com>
2022-08-25 01:25:36 +0000

    remove comments, add notes, tidy code

--- game.rb
+++ game.rb
@@ -8,39 +8,47 @@ class Game
     @p2 = p2
 
     puts "Welcome! Start a new game!"
-    
   end
 
+  #run game loop until @game_over == true
   def start()
     while @game_over == false
 
+      #run generate_question
       if @current_player == true
         self.generate_question(@p1)
       else
         self.generate_question(@p2)
       end
 
+      #check status of @game_over
       if @game_over == true
         self.game_over()
         #need to put Player x wins with a score of y/3
       else
+        #if game not over, switch current player and continue game loop to next turn
         puts "---NEW TURN---"
         @current_player = !@current_player
       end
     end    
   end
 
+  #generate questions
   def generate_question(player)
+    #create random numbers
     @num1 = rand(0..20)
     @num2 = rand(0..20)
     @operators = [:+, :-, :*, :/]
+    #grab a random operator
     @operator = @operators.sample()
     @answer = @num1.send(@operator, @num2)
    
     puts "#{player.name}: what does #{@num1} #{@operator} #{@num2} equal?"
 
+    #get player answer
     @input = gets.chomp
 
+    #check if answer is correct, adjust points accordingly
     if @input.to_i == @answer
       puts "#{player.name}: YES! You are correct!"
       puts "#{@p1.name}: #{@p1.score}/3 vs #{@p2.name}: #{@p2.score}/3"
@@ -50,17 +58,19 @@ class Game
       puts "#{@p1.name}: #{@p1.score}/3 vs #{@p2.name}: #{@p2.score}/3"
     end
 
+    #check player score
     if player.score  == 0
       @game_over = true
     end
   end
 
+  #run this function when the game is over (a player's score goes to 0).
   def game_over()
 
     puts "-----GAME OVER-----"
     puts "Good bye!"
   end
-
+  
 end
 
 
--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require './player.rb'
 require './game.rb'
-# require './question.rb'
 
+#get player names and initialize players
 puts "Player 1, what is your name?"
 name1 = gets.chomp
 player1 = Player.new(name1)
@@ -9,18 +9,6 @@ puts "Player 2, what is your name?"
 name2 = gets.chomp
 player2 = Player.new(name2)
 
+#start game loop
 game = Game.new(player1, player2)
 game.start
-
-
-
- 
-# Player, Question
-# Player input
-# Player, message
-# score: player1.score player2.score
-# NEW TURN
-# puts "-----New Turn-----"
-
-# loop through turns UNTIL one player runs out of lives. 
-# Each loop, check lives of each player, if one is at zero game over
